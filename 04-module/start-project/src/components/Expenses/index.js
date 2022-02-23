594cc633ff3ba4f27bed60531ef008951f2a03c3
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-02-23 16:23:13 -0300

    added 04 module

---
+++ 04-module/start-project/src/components/Expenses/index.js
@@ -0,0 +1,28 @@
+import { ExpenseItem } from './ExpenseItem'
+
+import './Expenses.css'
+
+export const Expenses = (props) => {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <div className='expenses'>
+        <ExpenseItem
+          title={props.items[0].title}
+          date={props.items[0].date}
+          amount={props.items[0].amount}
+        />
+        <ExpenseItem
+          title={props.items[1].title}
+          date={props.items[1].date}
+          amount={props.items[1].amount}
+        />
+        <ExpenseItem
+          title={props.items[2].title}
+          date={props.items[2].date}
+          amount={props.items[2].amount}
+        />
+    </div>
+    </div>
+  )
+}
