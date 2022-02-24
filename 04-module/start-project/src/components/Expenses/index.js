fd38434f5c46f512e4c6d8b828d96d0735344dad
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-02-24 12:05:48 -0300

    04-module: added ExpensesFilter component

--- 04-module/start-project/src/components/Expenses/index.js
+++ 04-module/start-project/src/components/Expenses/index.js
@@ -1,27 +1,28 @@
+import { ExpensesFilter } from './ExpenseFilter'
 import { ExpenseItem } from './ExpenseItem'
+import { Card } from '../UI/Card'
 
 import './Expenses.css'
 
 export const Expenses = (props) => {
   return (
-    <div>
-      <div className='expenses'>
-        <ExpenseItem
-          title={props.items[0].title}
-          date={props.items[0].date}
-          amount={props.items[0].amount}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          date={props.items[1].date}
-          amount={props.items[1].amount}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          date={props.items[2].date}
-          amount={props.items[2].amount}
-        />
-    </div>
-    </div>
+    <Card className='expenses'>
+      <ExpensesFilter />
+      <ExpenseItem
+        title={props.items[0].title}
+        date={props.items[0].date}
+        amount={props.items[0].amount}
+      />
+      <ExpenseItem
+        title={props.items[1].title}
+        date={props.items[1].date}
+        amount={props.items[1].amount}
+      />
+      <ExpenseItem
+        title={props.items[2].title}
+        date={props.items[2].date}
+        amount={props.items[2].amount}
+      />
+    </Card>
   )
 }
