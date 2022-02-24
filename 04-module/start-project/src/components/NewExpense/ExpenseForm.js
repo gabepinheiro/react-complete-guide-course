f6794b0f104047a2876e46723747ac05130e639e
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-02-24 10:31:34 -0300

    04-module: added handling form submission

--- 04-module/start-project/src/components/NewExpense/ExpenseForm.js
+++ 04-module/start-project/src/components/NewExpense/ExpenseForm.js
@@ -18,8 +18,20 @@ export const ExpenseForm = () => {
     setEnteredDate(e.target.value)
   }
 
+  const submitHandler = e => {
+    e.preventDefault()
+
+    const expenseDate = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
+    }
+
+    console.log(expenseDate)
+  }
+
   return (
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
           <div className="new-expense__control">
             <label htmlFor="title">Title</label>
