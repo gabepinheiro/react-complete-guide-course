ba08c52fde5efe68d4b702b6fa4fea8e6b3d371e
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-02-24 15:55:41 -0300

    22-module(react-auth): adding user signup

--- 22-module/react-auth/src/components/Auth/AuthForm.js
+++ 22-module/react-auth/src/components/Auth/AuthForm.js
@@ -1,25 +1,52 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const enteredEmail = useRef('')
+  const enteredPassword = useRef('')
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitHandler = async (e) => {
+    e.preventDefault()
+
+    if (!isLogin) {
+      try {
+        const r = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_API_KEY}`, {
+          method: 'POST',
+          body: JSON.stringify({
+            email: enteredEmail.current.value,
+            password: enteredPassword.current.value,
+            returnSecureToken: true
+          }),
+          headers:{
+            'Content-Type': 'application/json'
+          },
+        })
+
+        const result = await r.json()
+        console.log(result)
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+  }
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input type='email' id='email' required  ref={enteredEmail} />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input type='password' id='password' required ref={enteredPassword} />
         </div>
         <div className={classes.actions}>
           <button>{isLogin ? 'Login' : 'Create Account'}</button>
