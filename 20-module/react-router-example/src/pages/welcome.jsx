f25a4ee3621e05c77b8752ea6f4e96a440e07989
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-03-02 16:05:12 -0300

    20-module(react-router-example): working with nested routes

--- 20-module/react-router-example/src/pages/welcome.jsx
+++ 20-module/react-router-example/src/pages/welcome.jsx
@@ -1,7 +1,12 @@
+import { Route } from "react-router-dom"
+
 function Welcome () {
   return (
     <div>
       <h1>Welcome</h1>
+      <Route path="welcome/new-user">
+        <p>Welcome, new user!</p>
+      </Route>
     </div>
   )
 }
