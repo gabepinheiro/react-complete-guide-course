d6179ec1cddfb4859bb5ab18d45347d900069328
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-03-02 15:52:56 -0300

    20-module(react-router-example): using switch and exact for configuring routes

--- 20-module/react-router-example/src/App.jsx
+++ 20-module/react-router-example/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route } from "react-router-dom"
+import { Route, Switch } from "react-router-dom"
 import { MainHeader } from './layout/main-header'
 import Products from "./pages/products"
 import Welcome from "./pages/welcome"
@@ -9,15 +9,17 @@ export function App () {
     <div>
       <MainHeader />
       <main>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-        <Route path="/products">
-          <Products />
-        </Route>
-        <Route path="/product-detail/:productId">
-          <ProductDetail />
-        </Route>
+        <Switch>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
       </main>
     </div>
   )
--- 20-module/react-router-example/src/pages/products.jsx
+++ 20-module/react-router-example/src/pages/products.jsx
@@ -1,11 +1,19 @@
+import { Link } from "react-router-dom"
+
 function Products () {
   return (
     <div>
       <h1>Products</h1>
       <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
+        <li>
+          <Link to="/products/p1">A Book</Link>
+        </li>
+        <li>
+          <Link to="/products/p2">A Carpet</Link>
+        </li>
+        <li>
+          <Link to="/products/p3">An Online Course</Link>
+        </li>
       </ul>
     </div>
   )
