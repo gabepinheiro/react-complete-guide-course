b85cd474a8579982e14e57edd951b143934712e0
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-03-02 14:08:10 -0300

    20-module(react-router-example): working with Links

--- 20-module/react-router-example/src/App.jsx
+++ 20-module/react-router-example/src/App.jsx
@@ -1,10 +1,12 @@
 import { Route } from "react-router-dom"
+import { MainHeader } from './layout/main-header'
 import Products from "./pages/products"
 import Welcome from "./pages/welcome"
 
 export function App () {
   return (
     <div>
+      <MainHeader />
       <Route path="/welcome">
         <Welcome />
       </Route>
---
+++ 20-module/react-router-example/src/layout/main-header.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom"
+
+export function MainHeader () {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/welcome">Welcome</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
