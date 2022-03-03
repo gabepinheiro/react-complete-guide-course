a581cc453f3c5b9469d814b3f08b25cd39d97bb5
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-03-03 13:13:19 -0300

    20-module(great-quotes): implementing programatic navigation

--- 20-module/great-quotes/src/pages/new-quote.js
+++ 20-module/great-quotes/src/pages/new-quote.js
@@ -1,8 +1,13 @@
+import { useHistory } from 'react-router-dom'
 import QuoteForm from '../components/quotes/QuoteForm'
 
 function NewQuote () {
+  const history = useHistory()
+
   function addQuoteHandler (quoteData) {
     console.log(quoteData)
+
+    history.push('/quotes')
   }
 
   return (
