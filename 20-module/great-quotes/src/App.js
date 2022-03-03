69058cdfe6cab7b8b035b6b65d32085994365c17
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-03-03 09:45:54 -0300

    20-module(great-quotes): added routes

--- 20-module/great-quotes/src/App.js
+++ 20-module/great-quotes/src/App.js
@@ -1,7 +1,22 @@
+import { Route, Switch } from 'react-router-dom'
+import AllQuotes from './pages/all-quotes';
+import NewQuote from './pages/new-quote';
+import QuoteDetail from './pages/quote-detail';
+
 function App() {
   return (
     <div>
-
+      <Switch>
+        <Route path='/quotes' exact>
+          <AllQuotes />
+        </Route>
+        <Route path='/quotes/:quoteId'>
+          <QuoteDetail />
+        </Route>
+        <Route path='/new-quote'>
+          <NewQuote />
+        </Route>
+      </Switch>
     </div>
   );
 }
