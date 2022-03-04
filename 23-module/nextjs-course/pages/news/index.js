756004d8bd82de061d54374134256a10d9568bdb
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-03-04 15:31:38 -0300

    23-module(nextjs-course): linking between pages

--- 23-module/nextjs-course/pages/news/index.js
+++ 23-module/nextjs-course/pages/news/index.js
@@ -1,6 +1,22 @@
+import Link from 'next/link'
+
 function NewsPage () {
   return (
-    <h1>The News Page</h1>
+    <>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href='/news/nextjs-is-a-great-framework'>
+            Nextjs is a great Framework
+          </Link>
+        </li>
+        <li>
+          <Link href="news/something-else">
+            Something else
+          </Link>
+        </li>
+      </ul>
+    </>
   )
 }
 
