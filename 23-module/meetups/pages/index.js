689545ff7ab1e82ebd1a02d00986a2236466b2cf
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-03-04 16:42:23 -0300

    23-module(meetups): outputting a list of meetups

--- 23-module/meetups/pages/index.js
+++ 23-module/meetups/pages/index.js
@@ -1,6 +1,25 @@
+import MeetupList from '../components/meetups/MeetupList'
+
+const DUMMY_DATA = [
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/9/90/Stabil.JPG',
+    address: 'Some address 5, 12345 Some City',
+    description: 'This a first meetup!'
+  },
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/9/90/Stabil.JPG',
+    address: 'Some address 17, 12345 Some City',
+    description: 'This a second meetup!'
+  },
+]
+
 function HomePage () {
   return (
-    <h1>The HomePage Meetups</h1>
+    <MeetupList meetups={DUMMY_DATA}/>
   )
 }
 
