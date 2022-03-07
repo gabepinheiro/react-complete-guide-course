b9e3cce8bbce191defb0133fde1942be289a2442
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-03-07 10:25:08 -0300

    23-module(meetups): added custom component MeetupDetails and styles with css modules

--- 23-module/meetups/pages/[meetupId].js
+++ 23-module/meetups/pages/[meetupId].js
@@ -1,6 +1,13 @@
+import MeetupDetails from "../components/meetups/MeetupDetails"
+
 function DetailMeetupPage () {
   return (
-    <h1>The Detail Meetup Page</h1>
+    <MeetupDetails
+      image='https://upload.wikimedia.org/wikipedia/commons/9/90/Stabil.JPG'
+      title='A First Meetup'
+      address='Some Street 5, Some City'
+      description='The meetup description'
+    />
   )
 }
 
