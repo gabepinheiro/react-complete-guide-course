5984b6f3762952e858b5b153f2895a9dee01b32d
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-03-07 15:03:36 -0300

    23-module(meetups): sending http requests to API Routes

--- 23-module/meetups/pages/new-meetup.js
+++ 23-module/meetups/pages/new-meetup.js
@@ -1,8 +1,24 @@
+import { useRouter } from 'next/router'
+
 import NewMeetupForm from '../components/meetups/NewMeetupForm'
 
 function NewMeetupPage () {
-  function addMeetupHandler (enteredMeetup) {
-    console.log(enteredMeetup)
+  const router = useRouter()
+
+  async function addMeetupHandler (enteredMeetup) {
+    const response = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(enteredMeetup),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    const result = await response.json()
+
+    console.log(result)
+
+    router.push('/')
   }
 
   return (
