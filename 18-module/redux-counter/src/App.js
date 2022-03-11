69258bf5a815082129c8a2ae1046a42d07e3fcf9
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-03-10 23:05:39 -0300

    18-module(redux-counter): working with multiple slices

--- 18-module/redux-counter/src/App.js
+++ 18-module/redux-counter/src/App.js
@@ -1,8 +1,20 @@
+import { useSelector } from 'react-redux';
+
+import Header from './components/Header'
+import Auth from './components/Auth'
+import UserProfile from './components/UserProfile'
 import Counter from './components/Counter';
 
 function App() {
+  const isAuthenticated = useSelector(({ auth }) => auth.isAuthenticated)
+
   return (
-    <Counter />
+    <>
+      <Header />
+      {isAuthenticated && <UserProfile />}
+      {!isAuthenticated && <Auth />}
+     <Counter />
+    </>
   );
 }
 
--- 18-module/redux-counter/src/store/index.js
+++ 18-module/redux-counter/src/store/index.js
@@ -1,9 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+// Slices
 import { counterSlice } from './counter-slice'
+import { authSlice } from './auth-slice'
 
+// Actions
 export { counterActions } from './counter-slice'
+export {authActions  } from './auth-slice'
+
 
 export const store = configureStore({
-  reducer: counterSlice.reducer
+  reducer: {
+    counter: counterSlice.reducer,
+    auth: authSlice.reducer
+  }
 })
