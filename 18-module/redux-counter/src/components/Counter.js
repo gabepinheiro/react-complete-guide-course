4b846e888dd232a2ef90dcd4cfad692e933b97b4
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-03-10 21:37:55 -0300

    18-module(redux-counter): working with multiple state properties

--- 18-module/redux-counter/src/components/Counter.js
+++ 18-module/redux-counter/src/components/Counter.js
@@ -4,6 +4,7 @@ import classes from './Counter.module.css';
 
 const Counter = () => {
   const counter = useSelector(state => state.counter)
+  const showCounter = useSelector(state => state.showCounter)
   const dispatch = useDispatch()
 
   const incrementHandler = () => {
@@ -18,12 +19,14 @@ const Counter = () => {
     dispatch({type: 'DECREMENT'})
   }
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'TOGGLE'})
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={() => increaseHandler(10)}>IncreaseBy10</button>
--- 18-module/redux-counter/src/store/counter-reducer.js
+++ 18-module/redux-counter/src/store/counter-reducer.js
@@ -1,20 +1,29 @@
 const initialState = {
-  counter: 0
+  counter: 0,
+  showCounter: false
 }
 
 export const counterReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'INCREMENT':
       return {
-        counter: state.counter + 1
+        counter: state.counter + 1,
+        showCounter: true
       }
     case 'DECREMENT':
       return {
-        counter: state.counter - 1
+        counter: state.counter - 1,
+        showCounter: true
       }
     case 'INCREASE':
       return {
-        counter: state.counter + action.payload
+        counter: state.counter + action.payload,
+        showCounter: true
+      }
+    case 'TOGGLE':
+      return {
+        ...state,
+        showCounter: !state.showCounter
       }
     default:
       return state
